fbf79cb03c18fdaab34c4d4b934295e8946c48f6
Aleks B <aleksandroboko@gmail.com>
2017-12-19 17:01:03 +0200

    Add project files.

---
+++ GoodMedicalApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoodMedicalApp.Startup))]
+namespace GoodMedicalApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
