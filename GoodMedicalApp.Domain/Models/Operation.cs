894bf068ad077c6502111c661e61e3003537ebc9
Aleks B <aleksandroboko@gmail.com>
2017-12-20 17:59:39 +0200

    Add models in Domain

---
+++ GoodMedicalApp.Domain/Models/Operation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodMedicalApp.Domain.Models
+{
+    public class Operation
+    {
+        public int Id { get; set; }
+        public TypeOperation CurrentTypeOperation { get; set; }
+        public int TreatmentId { get; set; }
+        public ICollection<Medicine> Medicines { get; set; }
+    }
+}
---
+++ GoodMedicalApp.Domain/Models/TreatmentReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodMedicalApp.Domain.Models
+{
+    class TreatmentReport
+    {
+        public int Id { get; set; }
+        public Treatment CurrentTreatment { get; set; }
+        public string Conclusion { get; set; }
+        public string Comment { get; set; }
+        public ICollection<Medicine> Medicines { get; set; }
+    }
+}
